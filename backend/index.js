6d17a466d41daf1f87f8ceb52d934ada8836c277
Adam Hermansson <qurridutt@gmail.com>
2020-03-10 17:46:33 +0100

    added env variable for warcraftlogs api key, example-non-hidden file included

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 import express from 'express';
-import db from './db';
 
+const WARCRAFTLOGS_API_KEY = process.env.WARCRAFTLOGS_API_KEY;
 const app = express();
 const port = 3000;
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
