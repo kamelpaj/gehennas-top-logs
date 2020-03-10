2b49f5be03a62f739b5fccbe69291e63e568d2fd
Adam Hermansson <qurridutt@gmail.com>
2020-03-10 23:58:55 +0100

    nothing special

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 import express from 'express';
+import axios from 'axios';
 import {moltenCore, onyxia, blackwingLair} from './zones';
 
 const WARCRAFTLOGS_API_KEY = process.env.WARCRAFTLOGS_API_KEY;
+const baseUrl = 'https://classic.warcraftlogs.com:443/v1';
 const app = express();
 const port = 3000;
 
+console.log(Date.now())
 
+/*
+axios.get(`${baseUrl}/rankings/encounter/670?server=Gehennas&region=EU&page=1&api_key=${WARCRAFTLOGS_API_KEY}`).then( (response) => {
+    console.log(response.data);
+}).catch((response) => {
+    console.log(response);
+});
+*/
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
