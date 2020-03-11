a1c5afdc281b83cc1bfa7e590b369b175737d3a2
Adam Hermansson <qurridutt@gmail.com>
2020-03-11 20:02:01 +0100

    created cron job that runs at every raid reset

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 import express from 'express';
 import axios from 'axios';
 import {moltenCore, onyxia, blackwingLair} from './zones';
+const CronJob = require('cron').CronJob;
 
 const WARCRAFTLOGS_API_KEY = process.env.WARCRAFTLOGS_API_KEY;
 const baseUrl = 'https://classic.warcraftlogs.com:443/v1';
@@ -10,6 +11,14 @@ const port = 3000;
 
 console.log(Date.now())
 
+let lastReset = 1583910000;
+
+const job = new CronJob('0 8 * * 3', () => {
+    // Runs every week at Wednesday 08:00:00 CET
+  console.log('Reset time!');
+}, null, true, 'Europe/Stockholm');
+
+
 /*
 axios.get(`${baseUrl}/rankings/encounter/670?server=Gehennas&region=EU&page=1&api_key=${WARCRAFTLOGS_API_KEY}`).then( (response) => {
     console.log(response.data);
@@ -17,6 +26,6 @@ axios.get(`${baseUrl}/rankings/encounter/670?server=Gehennas&region=EU&page=1&ap
     console.log(response);
 });
 */
+job.start();
 app.get('/', (req, res) => res.send('Hello World!'));
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
